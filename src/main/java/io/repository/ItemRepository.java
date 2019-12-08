1e491784bd398559626ea480c200a7082ffd4424
KrzysztofPakaszewski <krzysztof.pakaszewski@gmail.com>
2019-12-08 13:53:19 +0100

    added matching-list and added queries for item that forces eager loading

--- src/main/java/io/repository/ItemRepository.java
+++ src/main/java/io/repository/ItemRepository.java
@@ -1,9 +1,13 @@
 package io.repository;
 import io.domain.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Spring Data  repository for the Item entity.
@@ -15,4 +19,10 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     @Query("select item from Item item where item.owner.login = ?#{principal.username}")
     List<Item> findByOwnerIsCurrentUser();
 
+    @Query(value = "select item from Item item join fetch item.owner",
+            countQuery = "select count(item) from Item item join item.owner")
+    Page<Item> findAll(Pageable pageable);
+
+    @Query("select item from Item item join fetch item.owner where item.id = ?1")
+    Optional<Item> findById( Long id);
 }
---
+++ src/main/webapp/app/entities/matching/matching-list.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import { ICrudGetAllAction } from 'react-jhipster';
+
+import { IRootState } from 'app/shared/reducers';
+import { getEntities } from './matching.reducer';
+import { IMatching } from 'app/shared/model/matching.model';
+import { APP_DATE_FORMAT, APP_LOCAL_DATE_FORMAT } from 'app/config/constants';
+import {MatchingComponent} from './matching-component';
+import {Box} from '@material-ui/core';
+import {Carousel,CarouselIndicators, CarouselItem,CarouselControl} from 'reactstrap';
+
+export interface IMatchingListProps extends StateProps, DispatchProps, RouteComponentProps<{ url: string }> {}
+
+export interface IMatchingListState{
+  activeIndex: number
+}
+
+
+
+export class MatchingList extends React.Component<IMatchingListProps, IMatchingListState> {
+
+  constructor(props){
+    super(props);
+    this.state= {
+        activeIndex: 0
+    }
+    this.previous = this.previous.bind(this);
+    this.next = this.next.bind(this);
+  }
+  componentDidMount() {
+    this.props.getEntities();
+  }
+
+  next (){
+      this.setState({
+          activeIndex: (this.state.activeIndex + 1)% this.props.list.length
+      });
+  };
+
+  previous(){
+      this.setState({
+          activeIndex: (this.state.activeIndex - 1)% this.props.list.length
+      });
+  };
+
+
+  goToIndex = newIndex =>{
+      this.setState({
+          activeIndex: newIndex
+      });
+  };
+
+  render() {
+    const { list } = this.props;
+    const {activeIndex} = this.state;
+    return (
+        <Box>
+            <Carousel
+              activeIndex = {activeIndex}
+              next = {this.next}
+              previous= {this.previous}
+              >
+              <CarouselIndicators items ={list} activeIndex={activeIndex} onClickHandler={this.goToIndex}/>
+                {list.map((item)=>{
+                    return(
+                        <CarouselItem
+                             key = {item.id}
+                        >
+                          {MatchingComponent(item)}
+                        </CarouselItem>
+                    );
+                })}
+                <CarouselControl direction= 'next' directionText='Next' onClickHandler = {this.next}/>
+                <CarouselControl direction= 'prev' directionText='Previous' onClickHandler = {this.previous}/>
+            </Carousel>
+        </Box>
+    );
+  }
+}
+
+const mapStateToProps = ({ matching }: IRootState) => ({
+  list: matching.entities
+});
+
+const mapDispatchToProps = {
+  getEntities
+};
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MatchingList);
