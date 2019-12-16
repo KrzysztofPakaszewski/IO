95e1f39663a9d6478a60a2f71473bacf424e4f07
psm8 <43780167+psm8@users.noreply.github.com>
2019-12-16 11:04:38 +0100

    Hide interested button after clicking on it

--- src/main/webapp/app/entities/search/item-detail.tsx
+++ src/main/webapp/app/entities/search/item-detail.tsx
@@ -31,7 +31,10 @@ export class ItemDetail extends React.Component<IItemDetailProps, IItemDetailSta
   }
 
   handleInterest() {
-    addNewInterest(this.props.itemEntity)
+    addNewInterest(this.props.itemEntity);
+    this.setState({
+      showInterestedButton: this.props.location.state == false
+    });
   }
 
   interestButtonLogic(itemEntity, currentUser) {
