f8cd6494fd31f75c4b599eaf9355b3548a16296c
KrzysztofPakaszewski <krzysztof.pakaszewski@gmail.com>
2020-01-05 16:13:15 +0100

    changing user signature

--- src/main/webapp/app/entities/user/user-component-minimized.tsx
+++ src/main/webapp/app/entities/user/user-component-minimized.tsx
@@ -1,28 +1,26 @@
 import React from 'react';
-import Rating from '@material-ui/lab/Rating';
-import Card from '@material-ui/core/Card';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import { Button} from 'reactstrap';
 import { Link } from 'react-router-dom';
-import {openFile} from "react-jhipster";
 
 
-// props: {score:Integer(0-5), login:String, review:String}
 export const UserCompMinimized = (userEntity) =>{
 
   return (
-    <Box maxWidth={600}>
-      <dl className="jh-entity-details">
-        <dt>
-          <span id="image"></span>
-        </dt>
-        <dd>{userEntity.image}</dd>
-        <dt>
-          <span id="login"></span>
-        </dt>
-        <dd>{userEntity.login}</dd>
-      </dl>
+    <Box width={80} component={Link} to={`/user/${userEntity.login}`}>
+      <Grid container
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        <Grid item>
+          <Box bgcolor="primary.main" color="primary.contrastText" width={80} height={80}/>
+        </Grid>
+        <Grid item>
+          {userEntity.login}
+        </Grid>
+      </Grid>
     </Box>
   );
 }
