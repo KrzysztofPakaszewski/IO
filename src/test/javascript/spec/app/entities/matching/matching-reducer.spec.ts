5beabab47bf8cc510df1991074232c79a1ae8d17
Sandra <sandra.giermanska@gmail.com>
2020-01-15 23:52:10 +0100

    fixing bug

--- src/test/javascript/spec/app/entities/matching/matching-reducer.spec.ts
+++ src/test/javascript/spec/app/entities/matching/matching-reducer.spec.ts
@@ -84,19 +84,18 @@ describe('Entities reducer tests', () => {
       );
     });
 
-    // TODO Fix bug
-    // it('should reset the state', () => {
-    //   expect(
-    //     reducer(
-    //       { ...initialState, loading: true },
-    //       {
-    //         type: ACTION_TYPES.RESET
-    //       }
-    //     )
-    //   ).toEqual({
-    //     ...initialState
-    //   });
-    // });
+    it('should reset the state', () => {
+      expect(
+        reducer(
+          { ...initialState, loading: true },
+          {
+            type: ACTION_TYPES.RESET
+          }
+        )
+      ).toEqual({
+        ...initialState
+      });
+    });
   });
 
   describe('Failures', () => {
